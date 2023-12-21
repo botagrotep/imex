4451077fb322977fc1397321515272cbbe939890
oleksandrfeseniuk <https://mail.mirohost.net/program1@agrotep.com>
2023-12-21 11:12:36 +0200

    export filter is added

--- src/main/resources/static/assets/js/import-export.js
+++ src/main/resources/static/assets/js/import-export.js
@@ -1,10 +1,12 @@
 const MANAGER_HIDDEN = 'manager-hidden';
+const EXPORT_HIDDEN = 'manager-hidden';
 
-const HIDDEN_CLASSES = [MANAGER_HIDDEN];
+const HIDDEN_CLASSES = [MANAGER_HIDDEN, EXPORT_HIDDEN];
 
 jQuery(document).ready(function () {
     addCommentEditor();
     addCurrentUserFilter();
+    addExportFilter();
 });
 
 function addCommentEditor() {
@@ -19,7 +21,6 @@ function addCommentEditor() {
 
 function addCurrentUserFilter() {
     const filterDiv = $('div#current-user-filter');
-    console.log(filterDiv);
     const currentUserId = parseInt($(filterDiv).find('p').text());
     const filterInput = $(filterDiv).find('input').each(function (n) {
         this.addEventListener('click', function (e) {
@@ -34,6 +35,32 @@ function addCurrentUserFilter() {
                                 $(parentTr).addClass(MANAGER_HIDDEN);
                             } else {
                                 $(parentTr).removeClass(MANAGER_HIDDEN);
+                                if (!HIDDEN_CLASSES.some((cls) => $(parentTr).hasClass(cls))) {
+                                    $(parentTr).removeAttr('hidden');
+                                }
+                            }
+                        }
+                    });
+        });
+    });
+}
+
+function addExportFilter() {
+    const filterDiv = $('div#export-filter');
+    const currentUserId = parseInt($(filterDiv).find('p').text());
+    const filterInput = $(filterDiv).find('input').each(function (n) {
+        this.addEventListener('click', function (e) {
+            const checkBoxState = this.checked;
+            $('div#transportations-div table tr td.direction span.unloading-country')
+                    .each(function () {
+                        const unloadingCountry = $(this).text().toString();
+                        if (unloadingCountry.toUpperCase() === 'UA') {
+                            const parentTr = $(this).closest('tr');
+                            if (checkBoxState) {
+                                $(parentTr).attr('hidden', 'hidden');
+                                $(parentTr).addClass(EXPORT_HIDDEN);
+                            } else {
+                                $(parentTr).removeClass(EXPORT_HIDDEN);
                                 if (!HIDDEN_CLASSES.some((cls) => $(parentTr).hasClass(cls))) {
                                     $(parentTr).removeAttr('hidden');
                                 }
